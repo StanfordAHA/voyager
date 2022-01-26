33c347cc7cd7f4eb9a4a576e20de55e1f3c4f5b5
KaiBartolone <kbartolo@stanford.edu>
2022-01-25 16:00:58 -0800

    pkl_parser quality of life changes
    Former-commit-id: d848a6a6757cfd6dce4a48cbd17fe9b87fe5f301

--- data/pkl_parser.py
+++ data/pkl_parser.py
@@ -1,8 +1,12 @@
+#!/usr/bin/python3
 import dill as pickle
 import numpy as np
 import torch
 import re
+import sys, getopt
+import os
 
+''' @brief: Returns size of flattened np array ''' 
 def get_layer_size(arr):
     result = 1
     dims = np.shape(arr)
@@ -10,6 +14,7 @@ def get_layer_size(arr):
         result *= num
     return result
 
+''' @brief: Returns number as byte array '''
 def itoba(num):
     arr = []
     for i in range(4):
@@ -19,6 +24,7 @@ def itoba(num):
     arr = reversed(arr)
     return arr
 
+''' @brief: Changes . to _ and fixes downsample layer names '''
 def clean_name(name):
     result = ""
     # if (not re.fullmatch("(.*bn2.*)|(.*bn1.*)|(.*downsample.*)", name)):
@@ -32,32 +38,48 @@ def clean_name(name):
     
     return result
 
-# folder = 'layer_data'
-infile = open("resnet8.pkl",'rb')
-new_dict = pickle.load(infile)
+if __name__ == "__main__":
+    # Get input file and output folder
+    argv = sys.argv[1:]
+    inputfile = ''
+    outputfolder = ''
+    try:
+       opts, args = getopt.getopt(argv,"hi:o:",["ifile=","ofile="])
+    except getopt.GetoptError:
+       print('usage: pkl_parser.py -i <inputfile> -o <outputfolder>')
+       sys.exit(2)
+    for opt, arg in opts:
+       if opt == '-h':
+          print('usage: pkl_parser.py -i <inputfile> -o <outputfolder>')
+          sys.exit()
+       elif opt in ("-i", "--ifile"):
+          inputfile = arg
+       elif opt in ("-o", "--ofile"):
+          outputfolder = arg
 
-for key, value in new_dict.items():
-    # print(key)
-    # print(get_layer_size(value))
+    # Create a new directory because it does not exist 
+    folder_exists = os.path.exists(os.getcwd() + "/" + outputfolder)
+    if not folder_exists:
+        os.makedirs(os.getcwd() + "/" + outputfolder)
 
-    filename = "resnet/" + clean_name(key)
-    print(filename)
-    with open(filename, 'wb') as f:
-        # f.write(bytearray(itoba(get_layer_size(value))))
-        byte_list = []
+    # Load pickle file and write binary layer files
+    infile = open(inputfile,'rb')
+    new_dict = pickle.load(infile)
+
+    for key, value in new_dict.items():
+        filename = outputfolder + "/" + clean_name(key)
+        print(filename)
+        with open(filename, 'wb') as f:
+            byte_list = []
         
-        for i in np.nditer(value):
-            if (i > 256):
-                print(i)
-            if (i < 0):
-                byte_list.append(i + 256)
-            else:
-                byte_list.append(i)
+            for i in np.nditer(value):
+                if (i > 127 or i < -128):
+                    raise NameError('Data is not byte sized')
+                if (i < 0):
+                    byte_list.append(i + 256)
+                else:
+                    byte_list.append(i)
 
-        f.write(bytearray(byte_list))
+            f.write(bytearray(byte_list))
 
-        
-
-        # f.write(bytearray([100, 100]))
-
-infile.close()
+    infile.close()
