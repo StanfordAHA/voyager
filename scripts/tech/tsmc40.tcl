8bea47298087e0b4b27f6dd8311e61ee84957edd
Kartik Prabhu <kprabhu7@stanford.edu>
2024-04-09 18:10:28 -0700

    Clean up and organize HLS scripts
    Former-commit-id: c8744c4cb2cc91a9636703b0d38364ef4520581b

---
+++ scripts/tech/tsmc40.tcl
@@ -0,0 +1,11 @@
+solution options set ComponentLibs/SearchPath {/home/shared/catapult/memories /home/shared/catapult/stdcells} -append
+solution library add tcbn40ulpbwp40_c170815tt0p9v25c_dc -- -rtlsyntool DesignCompiler -vendor TSMC -technology 40nm
+
+solution library add TS1N40LPB1024X64M4FW_wrapped
+solution library add TS1N40LPB1024X128M4FWBA_wrapped
+solution library add TS1N40LPB1024X256M4FWBA_wrapped
+solution library add TS1N40LPB1024X512M4FWBA_wrapped
+solution library add mem_1024x128
+solution library add mem_1024x256
+solution library add mem_1024x512
+solution library add mem_1024x1024
