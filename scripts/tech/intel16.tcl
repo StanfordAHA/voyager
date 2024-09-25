fc57f120e66f9744c9642eaffad61f88983b9fe1
Allen Pan <aifusenno1@gmail.com>
2024-09-25 14:15:57 -0700

    Support sp and parameterized memories
    Former-commit-id: f83d191ff2c38afc98c631b9a17654797487f165

--- scripts/tech/intel16.tcl
+++ scripts/tech/intel16.tcl
@@ -1,12 +1,8 @@
-solution options set ComponentLibs/SearchPath {/sim/kzf/birch/chip/catapult/memories /sim/kzf/birch/chip/catapult/stdcells} -append
+solution options set ComponentLibs/SearchPath {./memories /sim/kzf/birch/chip/catapult/stdcells} -append
 solution library add lib224_b15_7t_108pp_tttt_0p800v_25c_tttt_ctyp_ccslnt_dc -- -rtlsyntool DesignCompiler -vendor Intel -technology 16
 
-solution library add intel16_1024x256_rf_wrapper
-solution library add intel16_1024x256_sram_wrapper
-solution library add intel16_1024x512_rf_wrapper
+solution library add intel16_sram_wrapper_1024x64_1r1w
+solution library add intel16_sram_wrapper_1024x64_1rw
 
-#set memories(1024,128) "mem_1024x128.custom1024x128"
-set memories(sp,1024,256) "intel16_1024x256_sram_wrapper"
-set memories(dp,1024,256) "intel16_1024x256_rf_wrapper.intel16_1024x256_rf_wrapper"
-set memories(dp,1024,512) "intel16_1024x512_rf_wrapper.intel16_1024x512_rf_wrapper"
-#set memories(1024,1024) "mem_1024x1024.custom1024x1024"
+set memories(1r1w) "intel16_sram_wrapper_1024x64_1r1w.intel16_sram_wrapper_1024x64_1r1w"
+set memories(1rw)  "intel16_sram_wrapper_1024x64_1rw.intel16_sram_wrapper_1024x64_1rw"
