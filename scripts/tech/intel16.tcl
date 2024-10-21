6e86c1dac0f05c42c56f3813de8c094161d10753
Allen Pan <aifusenno1@gmail.com>
2024-10-21 11:35:07 -0700

    Update memory wrapper names
    Former-commit-id: f57860baf66683dada0222f396e0eb241d184631

--- scripts/tech/intel16.tcl
+++ scripts/tech/intel16.tcl
@@ -4,9 +4,9 @@ solution options set ComponentLibs/SearchPath {./memories /sim/kzf/birch/chip/ca
 solution library add lib224_b15_7t_108pp_tttt_0p800v_25c_tttt_ctyp_ccslnt_dc -- -rtlsyntool DesignCompiler -vendor Intel -technology 16
 
 catch {
-  solution library add intel16_sram_wrapper_1024x64_1r1w
-  solution library add intel16_sram_wrapper_1024x64_1rw
+  solution library add intel16_sram_wrapper_1r1w
+  solution library add intel16_sram_wrapper_1rw
 } res
 
-set memories(1r1w) "intel16_sram_wrapper_1024x64_1r1w.intel16_sram_wrapper_1024x64_1r1w"
-set memories(1rw)  "intel16_sram_wrapper_1024x64_1rw.intel16_sram_wrapper_1024x64_1rw"
+set memories(1r1w) "intel16_sram_wrapper_1r1w.intel16_sram_wrapper_1r1w"
+set memories(1rw)  "intel16_sram_wrapper_1rw.intel16_sram_wrapper_1rw"
