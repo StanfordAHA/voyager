28f4597d2f363d7eb7e807c75ea490f68120ba80
Kartik Prabhu <kprabhu7@stanford.edu>
2025-01-22 16:43:11 -0800

    Add ProcessingElement synthesis flow, make synthesis more flexible for different technologies
    Former-commit-id: 167e70ee2b30a2fdd1196df235f6fdd9a2589d9b

---
+++ physical-design/common/hls/run.sh
@@ -0,0 +1,35 @@
+set -o xtrace
+
+CATAPULT_BUILD_DIR=build/Catapult
+mkdir -p ${CATAPULT_BUILD_DIR}
+
+
+make network-proto NETWORK=resnet18 DATATYPE=${datatype}
+make ProcessingElement BUILD_DIR=build CATAPULT_BUILD_DIR=${CATAPULT_BUILD_DIR} DATATYPE=${datatype} OC_DIMENSION=16 IC_DIMENSION=16 INPUT_BUFFER_SIZE=1024 WEIGHT_BUFFER_SIZE=1024 ACCUM_BUFFER_SIZE=1024 CLOCK_PERIOD=${clock_period} TECHNOLOGY=${technology}
+
+[[ -f ${CATAPULT_BUILD_DIR}/ProcessingElement/ProcessingElement.v1/concat_rtl.v ]] || {
+  echo "Error: RTL generation failed"
+  exit 1
+}
+
+cd outputs
+ln -s ../build
+
+cp build/Catapult/ProcessingElement/ProcessingElement.v1/concat_rtl.v design.v
+
+# Renaming modules
+OLD_IFS=$IFS
+IFS=" "
+
+for vname in design.v; do
+  modname=$(grep -oP "(?<=module )ProcessingElement\w*" $vname | tail -1)
+  sed -i "s/\<$modname\>/ProcessingElement/g" $vname
+  modname=$(grep -oP "(?<=module )SystolicArray\w*" $vname | tail -1)
+  sed -i "s/\<$modname\>/SystolicArray/g" $vname
+  modname=$(grep -oP "(?<=module )VectorUnit\w*" $vname | tail -1)
+  sed -i "s/\<$modname\>/VectorUnit/g" $vname
+done
+
+IFS=$OLD_IFS
+
+cd ..
