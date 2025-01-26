d97b814bfe897944d4d85e4c9311c1620d7a91dc
Kartik Prabhu <kprabhu7@stanford.edu>
2025-01-25 16:45:30 -0800

    Disable memories for TSMC40
    Former-commit-id: cf564b6a0233f06b0754cd678c3aec3d48e8bd83

--- scripts/tech/tsmc40.tcl
+++ scripts/tech/tsmc40.tcl
@@ -1,14 +1,18 @@
 solution options set ComponentLibs/SearchPath {/home/shared/catapult/memories /home/shared/catapult/stdcells} -append
 solution library add tcbn40ulpbwp40_c170815tt0p9v25c_dc -- -rtlsyntool DesignCompiler -vendor TSMC -technology 40nm
 
-solution library add TS1N40LPB1024X64M4FW_wrapped
-solution library add TS1N40LPB1024X128M4FWBA_wrapped
-solution library add TS1N40LPB1024X256M4FWBA_wrapped
-solution library add TS1N40LPB1024X512M4FWBA_wrapped
-solution library add mem_1024x128
-solution library add mem_1024x256
-solution library add mem_1024x512
-solution library add mem_1024x1024
+
+# TODO: fix these memories for TSMC 40nm
+solution library add ccs_sample_mem
+
+# solution library add TS1N40LPB1024X64M4FW_wrapped
+# solution library add TS1N40LPB1024X128M4FWBA_wrapped
+# solution library add TS1N40LPB1024X256M4FWBA_wrapped
+# solution library add TS1N40LPB1024X512M4FWBA_wrapped
+# solution library add mem_1024x128
+# solution library add mem_1024x256
+# solution library add mem_1024x512
+# solution library add mem_1024x1024
 
 set memories(dp,1024,128) "mem_1024x128.custom1024x128"
 set memories(dp,1024,256) "mem_1024x256.custom1024x256"
