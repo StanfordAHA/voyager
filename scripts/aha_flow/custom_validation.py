9ec6a33bce4cde7cec1a96b295daef12a219c975
Michael Oduoza <mcoduoza@gmail.com>
2026-04-01 17:15:01 -0700

    Bring up llama_prefill silu layer

--- scripts/aha_flow/custom_validation.py
+++ scripts/aha_flow/custom_validation.py
@@ -10,7 +10,7 @@ import torch.nn.functional as F
 import argparse
 import json
 
-from parse_dnnLayer_tensors import float32_to_bfloat16_bits
+# from parse_dnnLayer_tensors import float32_to_bfloat16_bits
 from parse_dnnLayer_tensors import write_list_to_hex
 from parse_dnnLayer_tensors import read_tensor
 
@@ -31,16 +31,19 @@ def verify_bert_gelu():
 
     # Convert output to float32 so can use float32_to_bfloat16_bits
     output_bf16_approx = output_bf16_approx.to(torch.float32)
-    output_bf16_approx = float32_to_bfloat16_bits(output_bf16_approx)
-    output_bf16_approx = output_bf16_approx.flatten().tolist()
+    # output_bf16_approx = float32_to_bfloat16_bits(output_bf16_approx)
+    # output_bf16_approx = output_bf16_approx.flatten().tolist()
 
-    # Write output tensor to hex file
-    write_list_to_hex(output_bf16_approx, '/aha/voyager/gold_activation.txt', "", add_metadata=False)
+    return output_bf16_approx, "bfloat16"
+
+    # # Write output tensor to hex file
+    # write_list_to_hex(output_bf16_approx, '/aha/voyager/gold_activation.txt', "", add_metadata=False)
 
 
 def verify_llama_prefill_silu():
+    print("Running custom validation for SiLU approximation on LLaMA prefill...")
     # Read input tensor
-    input_path = "/aha/voyager/test/compiler/networks/llama/prefill/MXINT8/tensor_files/linear_mx_default_4.bin"
+    input_path = "/aha/voyager/test/compiler/networks/llama_prefill/MXINT8/tensor_files/linear_mx_default_4.bin"
     input_shape = (512, 8192)
     input_tensor = read_tensor(input_path, input_shape)
 
@@ -54,11 +57,13 @@ def verify_llama_prefill_silu():
 
     # Convert output to float32 so can use float32_to_bfloat16_bits
     output_bf16_approx = output_bf16_approx.to(torch.float32)
-    output_bf16_approx = float32_to_bfloat16_bits(output_bf16_approx)
-    output_bf16_approx = output_bf16_approx.flatten().tolist()
+    # output_bf16_approx = float32_to_bfloat16_bits(output_bf16_approx)
+    # output_bf16_approx = output_bf16_approx.flatten().tolist()
 
-    # Write output tensor to hex file
-    write_list_to_hex(output_bf16_approx, '/aha/voyager/gold_activation.txt', "", add_metadata=False)
+    return output_bf16_approx, "bfloat16"
+
+    # # Write output tensor to hex file
+    # write_list_to_hex(output_bf16_approx, '/aha/voyager/gold_activation.txt', "", add_metadata=False)
 
 
 def verify_bert_tanh():
@@ -67,11 +72,13 @@ def verify_bert_tanh():
     gold_shape = (1, 768)
     gold_tensor = read_tensor(gold_path, gold_shape)
 
-    gold_tensor_bf16 = float32_to_bfloat16_bits(gold_tensor)
-    gold_tensor_bf16 = gold_tensor_bf16.flatten().tolist()
+    # gold_tensor_bf16 = float32_to_bfloat16_bits(gold_tensor)
+    # gold_tensor_bf16 = gold_tensor_bf16.flatten().tolist()
 
-    # Write output tensor to hex file
-    write_list_to_hex(gold_tensor_bf16, '/aha/voyager/gold_activation.txt', "", add_metadata=False)
+    return gold_tensor, "bfloat16"
+
+    # # Write output tensor to hex file
+    # write_list_to_hex(gold_tensor_bf16, '/aha/voyager/gold_activation.txt', "", add_metadata=False)
 
 def verify_resnet18_submodule():
     # Read gold tensor
@@ -107,11 +114,13 @@ def verify_resnet18_submodule():
         else:
             raise NotImplementedError("X dimension host tiling is not supported for non 3-D tensors yet.")
 
-    gold_tensor_bf16 = float32_to_bfloat16_bits(gold_tensor)
-    gold_tensor_bf16 = gold_tensor_bf16.flatten().tolist()
+    # gold_tensor_bf16 = float32_to_bfloat16_bits(gold_tensor)
+    # gold_tensor_bf16 = gold_tensor_bf16.flatten().tolist()
 
-    # Write output tensor to hex file
-    write_list_to_hex(gold_tensor_bf16, '/aha/voyager/gold_activation.txt', "", add_metadata=False)
+    return gold_tensor, "bfloat16"
+
+    # # Write output tensor to hex file
+    # write_list_to_hex(gold_tensor_bf16, '/aha/voyager/gold_activation.txt', "", add_metadata=False)
 
 def verify_fakegemm_linear():
     # Read gold tensor
@@ -119,32 +128,50 @@ def verify_fakegemm_linear():
     gold_shape = (3364, 32)
     gold_tensor = read_tensor(gold_path, gold_shape)
 
-    gold_tensor_bf16 = float32_to_bfloat16_bits(gold_tensor)
-    gold_tensor_bf16 = gold_tensor_bf16.flatten().tolist()
+    # gold_tensor_bf16 = float32_to_bfloat16_bits(gold_tensor)
+    # gold_tensor_bf16 = gold_tensor_bf16.flatten().tolist()
 
-    # Write output tensor to hex file
-    write_list_to_hex(gold_tensor_bf16, '/aha/voyager/gold_activation.txt', "", add_metadata=False)
+    return gold_tensor, "bfloat16"
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Custom validation scripts for AHA Voyager flows.")
-    # Add model and layer args
-    parser.add_argument("--model", required=True, help="Model name (e.g., bert, resnet18)")
-    parser.add_argument("--layer", required=True, help="Layer name (e.g., linear_mx_default_4)")
+    # # Write output tensor to hex file
+    # write_list_to_hex(gold_tensor_bf16, '/aha/voyager/gold_activation.txt', "", add_metadata=False)
 
-    args = parser.parse_args()
-    model = args.model
-    layer = args.layer
+# if __name__ == "__main__":
+#     parser = argparse.ArgumentParser(description="Custom validation scripts for AHA Voyager flows.")
+#     # Add model and layer args
+#     parser.add_argument("--model", required=True, help="Model name (e.g., bert, resnet18)")
+#     parser.add_argument("--layer", required=True, help="Layer name (e.g., linear_mx_default_4)")
 
+#     args = parser.parse_args()
+#     model = args.model
+#     layer = args.layer
+
+#     if model == "bert" and (layer == "linear_mx_default_4" or layer == "gelu"):
+#         verify_bert_gelu()
+#     elif model == "llama_prefill" and (layer == "linear_mx_default_4" or layer == "silu"):
+#         verify_llama_prefill_silu()
+#     elif model == "bert" and layer == "tanh":
+#         verify_bert_tanh()
+#     elif model == "fakegemm" and layer == "linear_default_1":
+#         verify_fakegemm_linear()
+#     elif model == "resnet18" and layer == "submodule":
+#         verify_resnet18_submodule()
+#     else:
+#         raise NotImplementedError(f"No custom validation script for model {model} layer {layer}")
+
+
+def do_custom_validation(model, layer):
     if model == "bert" and (layer == "linear_mx_default_4" or layer == "gelu"):
-        verify_bert_gelu()
+        return verify_bert_gelu()
     elif model == "llama_prefill" and (layer == "linear_mx_default_4" or layer == "silu"):
-        verify_llama_prefill_silu()
+        return verify_llama_prefill_silu()
     elif model == "bert" and layer == "tanh":
-        verify_bert_tanh()
+        return verify_bert_tanh()
     elif model == "fakegemm" and layer == "linear_default_1":
-        verify_fakegemm_linear()
+        return verify_fakegemm_linear()
     elif model == "resnet18" and layer == "submodule":
-        verify_resnet18_submodule()
+        return verify_resnet18_submodule()
     else:
         raise NotImplementedError(f"No custom validation script for model {model} layer {layer}")
+
 
